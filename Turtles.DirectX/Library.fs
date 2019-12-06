2160c50551f90c7dc56d7cfa7d2d0c30b905b8d6
Bjørn Einar Bjartnes <Bjorn.Einar.Bjartnes@nrk.no>
2019-12-07 00:22:13 +0100

    working more

--- Turtles.DirectX/Library.fs
+++ Turtles.DirectX/Library.fs
@@ -14,7 +14,7 @@ open SharpDX.Direct2D1.Effects
 [<EntryPoint>]
 let main argv = 
     // 24 x 15
-    let form = new RenderForm("SharpDX - MiniTri Direct2D - Direct3D 10 Sample", Size = new Size(1500, 800))
+    let form = new RenderForm("SharpDX - MiniTri Direct2D - Direct3D 10 Sample", Size = Size(1500, 800))
     let desc = SwapChainDescription (
                 BufferCount = 1,
                 ModeDescription =
@@ -29,13 +29,13 @@ let main argv =
     SharpDX.Direct3D10.Device1.CreateWithSwapChain(DriverType.Hardware, DeviceCreationFlags.BgraSupport, desc,
                 SharpDX.Direct3D10.FeatureLevel.Level_10_1, &device, swapChain) 
 
-    use d2DFactory = new SharpDX.Direct2D1.Factory()
+    use d2DFactory = new Direct2D1.Factory()
     use factory = (!swapChain).GetParent<SharpDX.DXGI.Factory>()
     factory.MakeWindowAssociation(form.Handle, WindowAssociationFlags.IgnoreAll)
-    use backBuffer = SharpDX.Direct3D10.Resource.FromSwapChain<Texture2D>((!swapChain), 0)
+    use backBuffer = Resource.FromSwapChain<Texture2D>((!swapChain), 0)
     use renderTrgt = new RenderTargetView(device, backBuffer)
     let surface = backBuffer.QueryInterface<Surface>()
-    let d2DRenderTarget = new RenderTarget(d2DFactory, surface, RenderTargetProperties(PixelFormat(Format.Unknown, SharpDX.Direct2D1.AlphaMode.Premultiplied)))
+    let d2DRenderTarget = new RenderTarget(d2DFactory, surface, RenderTargetProperties(PixelFormat(Format.Unknown, Direct2D1.AlphaMode.Premultiplied)))
     
     let pinkBrush = new SolidColorBrush(d2DRenderTarget, Interop.RawColor4(255.0f,10.0f,10.0f,100.0f)) 
     let j = ref 0.0
@@ -45,19 +45,23 @@ let main argv =
             j := rot + 0.001
             d2DRenderTarget.BeginDraw()
             let myAngle : float32 = float32 rot 
-            let center = new SharpDX.Vector2(float32 500.0,float32 300.0)
-            let scale = new SharpDX.Vector2(float32 (-rot+1.0),float32 (-rot+1.0))
-//            d2DRenderTarget.Transform <- Matrix3x2.Scaling(scale)*Matrix3x2.Translation(-center)*Matrix3x2.Rotation(myAngle)*Matrix3x2.Translation(center)
+            let center = SharpDX.Vector2(float32 500.0,float32 300.0)
+            let scale = SharpDX.Vector2(float32 (-rot+1.0),float32 (-rot+1.0))
+            let mtrx = (Matrix3x2.Scaling(scale)*Matrix3x2.Translation(-center)*Matrix3x2.Rotation(myAngle)*Matrix3x2.Translation(center)) 
+            // could not find a way to implicit cast from matrix to rawmatrix
+            let mtrxa = Interop.RawMatrix3x2(mtrx.M11, mtrx.M12, mtrx.M21, mtrx.M22, mtrx.M31, mtrx.M32)
+            d2DRenderTarget.Transform <- mtrxa 
+
             let printLines (lines:seq<Line option*Turtle>) = 
                 let printLine (l: Line) = 
                     let ((x1,y1),(x2,y2)) = l 
-//                    d2DRenderTarget.DrawLine(new Vector2(x1,y1),new Vector2(x2,y2), pinkBrush,0.3f) 
+                    d2DRenderTarget.DrawLine(Interop.RawVector2(x1,y1),Interop.RawVector2(x2,y2), pinkBrush,0.3f) 
                     d2DRenderTarget.DrawLine(Interop.RawVector2(x1,y1),Interop.RawVector2(x2,y2), pinkBrush,0.2f) 
                 for line,_ in lines do
                     match line with 
                         | None -> ()
                         | Some(l) -> printLine l 
-//            d2DRenderTarget.Clear(new Nullable<Interop.RawColor4>(SharpDX.RawColor4(0.0f, 0.0f, 0.0f, 100.0f)))
+            d2DRenderTarget.Clear(new Nullable<Interop.RawColor4>(Interop.RawColor4(0.0f, 0.0f, 0.0f, 100.0f)))
 //            largeSimpleTurtle 271 (0.0, (float 500.0,300.0)) |> printLines
             for x in 1 ..1.. 24 do // 24*15 = 360
                 for y in 1 ..2.. 15 do
