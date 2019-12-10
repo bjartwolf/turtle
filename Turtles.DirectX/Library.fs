62893540673ea72053ab0e186637b1b88cdede2c
Bjørn Einar Bjartnes <Bjorn.Einar.Bjartnes@nrk.no>
2019-12-10 10:54:05 +0100

    groups

--- Turtles.DirectX/Library.fs
+++ Turtles.DirectX/Library.fs
@@ -15,7 +15,7 @@ open SharpDX.Direct2D1.Effects
 [<EntryPoint>]
 let main argv = 
     // 24 x 15
-    let form = new RenderForm("SharpDX - MiniTri Direct2D - Direct3D 10 Sample", Size = Size(2000, 800))
+    let form = new RenderForm("Fish", Size = Size(2000, 800))
 
     let desc = SwapChainDescription (
                 BufferCount = 1,
@@ -46,41 +46,65 @@ let main argv =
     
     let pink = Interop.RawColor4(hotpink.X, hotpink.Y, hotpink.Z, 50.0f)
     
-    let pinkBrush = new SolidColorBrush(d2DRenderTarget, pink, BrushProperties(Opacity = 0.01f) |> Nullable<BrushProperties>)
+    let pinkBrush = new SolidColorBrush(d2DRenderTarget, pink, BrushProperties(Opacity = 0.02f) |> Nullable<BrushProperties>)
 
-    let getTransFormMatrix (scale:float32) (translationx: float32) (translationy: float32)= 
-        let center = Vector2(translationx, translationy)
-        let scale = Vector2(scale, scale)
-        let mtrx = Matrix3x2.Translation(center)*(Matrix3x2.Scaling(scale))
+    let matrixToRaw (mtrx: Matrix3x2) =
         Interop.RawMatrix3x2(mtrx.M11, mtrx.M12, mtrx.M21, mtrx.M22, mtrx.M31, mtrx.M32)
 
-    let mutable x = 0.0f
+    let rotate (angleInRads:float32) = 
+         Matrix3x2.Rotation(angleInRads)
+    let translate (translationx: float32) (translationy: float32)= 
+        let center = Vector2(translationx, translationy)
+        Matrix3x2.Translation(center)
+    let scale (scale:float32) = 
+        let scale = Vector2(scale, scale)
+        Matrix3x2.Scaling(scale)
+    let verticalFlip = 
+        let scale = Vector2(-1.0f, 1.0f)
+        Matrix3x2.Scaling(scale)
+    let horizontalFlip = 
+        let scale = Vector2(1.0f, -1.0f)
+        Matrix3x2.Scaling(scale)
+     
+    let geo = new PathGeometry(d2DFactory)
+    let sink = geo.Open()
+    for (start, bezierCurve) in hendersonFishCurves do
+        sink.BeginFigure(start, FigureBegin.Hollow)
+        sink.AddBezier(bezierCurve)
+        sink.EndFigure(FigureEnd.Open)
+    let foo = sink.Close()
+    let drawFish geo = d2DRenderTarget.DrawGeometry(geo, pinkBrush, strokeWidth = 1.0f)
     RenderLoop.Run(form, fun _ ->
             d2DRenderTarget.BeginDraw()
-
-            let geo = new PathGeometry(d2DFactory)
-            let sink = geo.Open()
-            x <- x + 0.1f
- //           sink.BeginFigure(Interop.RawVector2(0.0f, 0.0f), FigureBegin.Hollow)
-//            d2DRenderTarget.Transform <- getTransFormMatrix x 3.0f 1.0f
-
-
-//            addFishToSink sink
-  //          sink.EndFigure(FigureEnd.Open)
-            for (start, bezierCurve) in hendersonFishCurves do
-                sink.BeginFigure(start, FigureBegin.Hollow)
-                sink.AddBezier(bezierCurve)
-                sink.EndFigure(FigureEnd.Open)
-            let foo = sink.Close()
+            d2DRenderTarget.Transform <- translate 1000.0f 400.0f |> matrixToRaw
+            let fishSize = 100.0f
             //d2DRenderTarget.Clear(new Nullable<Interop.RawColor4>(Interop.RawColor4(0.0f, 0.0f, 0.0f, 0.90f)))
+            let largerFish = new TransformedGeometry(d2DFactory, geo, (scale fishSize |> matrixToRaw)) 
 
-            let foo = new TransformedGeometry(d2DFactory, geo, (getTransFormMatrix (100.0f+x) 3.0f 1.0f)) 
-            let foo2 = new TransformedGeometry(d2DFactory, geo, (getTransFormMatrix (x + 200.0f) 1.0f 1.0f)) 
+            let rotFish  = new TransformedGeometry(d2DFactory, largerFish, (rotate (float32 (Math.PI/2.0))
+                                                                          * translate fishSize fishSize |> matrixToRaw))
 
-            d2DRenderTarget.DrawGeometry(geo, pinkBrush, strokeWidth = 2.0f)
-            d2DRenderTarget.DrawGeometry(foo, pinkBrush, strokeWidth = 2.0f)
-            d2DRenderTarget.DrawGeometry(foo2, pinkBrush, strokeWidth = 2.0f)
- 
+            let flipFish2 = new TransformedGeometry(d2DFactory, largerFish, (horizontalFlip
+                                                                           * verticalFlip
+                                                                           * translate fishSize fishSize |> matrixToRaw)) 
+
+            let flipFish3 = new TransformedGeometry(d2DFactory, largerFish, (verticalFlip |> matrixToRaw)) 
+            let flipFish4 = new TransformedGeometry(d2DFactory, largerFish, (horizontalFlip |> matrixToRaw)) 
+
+            drawFish largerFish 
+            drawFish rotFish
+            drawFish flipFish2
+            drawFish flipFish3
+            drawFish flipFish4
+            let group = new GeometryGroup(d2DFactory, Direct2D1.FillMode.Alternate, [|largerFish;
+                                                                                      rotFish;
+                                                                                      flipFish2;
+                                                                                      flipFish3|])   
+            let movedGroup = new TransformedGeometry(d2DFactory, group, translate fishSize fishSize |> matrixToRaw)  
+
+            drawFish movedGroup 
+//            drawFish flipFish2
+
             d2DRenderTarget.EndDraw()
             (!swapChain).Present(0, PresentFlags.None) |> ignore
         )
@@ -88,7 +112,6 @@ let main argv =
     backBuffer.Dispose()
     device.ClearState()
     device.Flush()
-    device.Dispose()
     device.Dispose()
     (!swapChain).Dispose()
     0 // return an integer exit code
