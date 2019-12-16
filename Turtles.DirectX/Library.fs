8151ec5c102a388b7b7bd670fe72b5d27011d376
Bjørn Einar Bjartnes <bjartnes@gmail.com>
2019-12-16 21:22:03 +0100

    onto something

--- Turtles.DirectX/Library.fs
+++ Turtles.DirectX/Library.fs
@@ -17,7 +17,7 @@ open SharpDX.Direct2D1.Effects
 [<EntryPoint>]
 let main argv = 
     // 24 x 15
-    let form = new RenderForm("Fish", Size = Size(2000, 800))
+    let form = new RenderForm("Fish", Size = Size(1200, 1200))
 
     let desc = SwapChainDescription (
                 BufferCount = 1,
@@ -53,38 +53,13 @@ let main argv =
     let matrixToRaw (mtrx: Matrix3x2) =
         Interop.RawMatrix3x2(mtrx.M11, mtrx.M12, mtrx.M21, mtrx.M22, mtrx.M31, mtrx.M32)
 
-    let rotate (angleInRads:float32) = 
-         Matrix3x2.Rotation(angleInRads)
-    let rotate270deg =
-        rotate (float32 Math.PI*1.5f)
-    let rotate180deg =
-        rotate (float32 Math.PI)
-    let rotate90deg =
-        rotate ((float32 Math.PI)/2.0f)
-    let rotate45deg =
-        rotate ((float32 Math.PI)/4.0f)
-    let translatey (translationy: float32) = 
-        let center = Vector2(0.0f, translationy)
-        Matrix3x2.Translation(center)
-    let translatex (translationx: float32) = 
-        let center = Vector2(translationx, 0.0f)
-        Matrix3x2.Translation(center)
     let translate (translationx: float32) (translationy: float32)= 
         let center = Vector2(translationx, translationy)
         Matrix3x2.Translation(center)
     let scale (scalex:float32) (scaley: float32)= 
         let scale = Vector2(scalex, scaley)
         Matrix3x2.Scaling(scale)
-    let scaleequal (scale:float32) = 
-        let scale = Vector2(scale, scale)
-        Matrix3x2.Scaling(scale)
-    let flip = 
-        let scale = Vector2(1.0f, -1.0f)
-        Matrix3x2.Scaling(scale)
-    let mirror = 
-        let scale = Vector2(-1.0f, 1.0f)
-        Matrix3x2.Scaling(scale)
-     
+    
     let fishGeo = new PathGeometry(d2DFactory)
     let sink = fishGeo.Open()
     for (start, bezierCurve) in hendersonFishCurves do
@@ -93,7 +68,6 @@ let main argv =
         sink.AddBezier(bezierCurve)
         sink.EndFigure(FigureEnd.Open)
     let foo = sink.Close()
-//    let drawFish geo = d2DRenderTarget.DrawGeometry(geo, pinkBrush, strokeWidth = 1.0f)
 
     let drawPicture (picture: Picture) (box: Box)= 
         d2DRenderTarget.DrawGeometry(picture box, pinkBrush, strokeWidth = 1.0f)
@@ -108,86 +82,33 @@ let main argv =
     RenderLoop.Run(form, fun _ ->
             //d2DRenderTarget.Clear(new Nullable<Interop.RawColor4>(Interop.RawColor4(0.0f, 0.0f, 0.0f, 0.90f)))
             d2DRenderTarget.BeginDraw()
-            d2DRenderTarget.Transform <- translate 1000.0f 400.0f |> matrixToRaw
+//            d2DRenderTarget.Transform <- translate 1000.0f 400.0f |> matrixToRaw
             let group = grouper d2DFactory 
 
-            let fishSize = 100.0f
-            let createBox fishSize =  
+            let createBox size =  
                 { a = Vector(0.0f, 0.0f); 
-                  b = Vector(fishSize, 0.0f); 
-                  c = Vector(0.0f, fishSize)}
-            let box100 = createBox fishSize
+                  b = Vector(size, 0.0f); 
+                  c = Vector(0.0f, size)}
+            let box1000 = createBox 1000.0f 
+            let box100 = createBox 100.0f 
+            let box10 = createBox 10.0f 
             let box1 = createBox 1.0f 
 
-            let geoInBox (box: Box) (geo: Geometry): Geometry =
-                let transform : Matrix3x2 -> Geometry -> Geometry = transformer d2DFactory 
-                transform ((scale box.b.X box.c.Y) * (translate box.a.X box.a.Y)) geo
-
-            let geoInBox' (geo: Geometry) (box: Box) : Geometry =
+            let geoInBox (geo: Geometry) (box: Box) : Geometry =
                 let transform : Matrix3x2 -> Geometry -> Geometry = transformer d2DFactory 
                 transform ((scale box.b.X box.c.Y) * (translate box.a.X box.a.Y)) geo
    
-            let drawInBox (box: Box) (geo: Geometry) =
-                let g = geoInBox box geo 
-                d2DRenderTarget.DrawGeometry(g, pinkBrush, strokeWidth = 1.0f)
+            let draw (geo: Geometry) =
+                d2DRenderTarget.DrawGeometry(geo, pinkBrush, strokeWidth = 1.0f)
 
             let baz = getThings group 
 
-            // pic er en funksjon fra box til geometri
-            //let pic = fun (b:Box) -> fishGeo :> Geometry 
-            let tile = baz.ttile (geoInBox' fishGeo) 
-            let baar = baz.utile tile 
-            drawInBox box1 (baar box100)
+            let f = geoInBox fishGeo
+//            let q = baz.squareLimit 4 f
+//            let tile = baz.ttile (geoInBox' fishGeo) 
+            let baar = baz.utile f 
+            draw (baar box1000)
 
-//            drawPicture fishPic box100 
-
-(*
-
-            let beside (g1: Geometry) (g2: Geometry) =
-                let deltaG1x = 100.0f 
-                group [|g1;(transform (translatex deltaG1x) g2)|] 
-            let fishSize = 100.0f
-            let fishGeo = transform (scale fishSize * (translate 50.0f 50.0f) * rotate180deg * mirror * translatex 50.0f) geo
-
-*)
-(*
-    
-            let fish = (createVector origo (fishSize, 0.0f) (0.0f, fishSize), fishGeo)
-            *)
-//            let fish2 = transform (rotate45deg * (scale (1.0f/1.41f))* flip * translatey (50.0f/1.41f)) fishGeo
-//            let fish3 = transform (rotate270deg * (translatey 100.0f) ) fish2
-
-//            let ttile = over fishGeo (over fish2 fish3)
-//            drawFish ttile 
-//            let figure5 = over fish (transform rotate180deg fish)
-//            drawFish figure5
-
-(*
-            let rotFish  = transform (rotate deg90 
-                                    * translate fishSize fishSize )
-                                      largerFish 
-            let flipFish2 = transform (horizontalFlip
-                                     * verticalFlip
-                                     * translate fishSize fishSize) 
-                                     largerFish
-
-            let flipFish3 = transform verticalFlip largerFish 
-            let flipFish4 = transform horizontalFlip largerFish 
-
-            drawFish largerFish 
-            drawFish rotFish
-            drawFish flipFish2
-            drawFish flipFish3
-            drawFish flipFish4
-            let fishgroup = group [|largerFish;
-                                    rotFish;
-                                    flipFish2;
-                                    flipFish3 |]   
-
-            let movedGroup = transform (translate fishSize fishSize) fishgroup
-
-            drawFish movedGroup 
-*)
             d2DRenderTarget.EndDraw()
             (!swapChain).Present(0, PresentFlags.None) |> ignore
         )
