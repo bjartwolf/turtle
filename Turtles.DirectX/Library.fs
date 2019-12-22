3e11c88f5f7720f2507aa2f9532913bbfe517386
Bjørn Einar Bjartnes <bjartnes@gmail.com>
2019-12-22 10:34:56 +0100

    squere limit

--- Turtles.DirectX/Library.fs
+++ Turtles.DirectX/Library.fs
@@ -74,7 +74,7 @@ let main argv =
     let emptySink = emptyGeo.Open()
     emptySink.Close()
 
-    let fishGeo = new PathGeometry(d2DFactory)
+    let fishGeo = new PathGeometry(d2DFactory) 
     let sink = fishGeo.Open()
     sink.SetFillMode(Direct2D1.FillMode.Alternate)
     for (start, bezierCurve) in hendersonFishCurves do
@@ -83,20 +83,20 @@ let main argv =
         sink.AddBezier(bezierCurve)
         sink.EndFigure(FigureEnd.Open)
     let foo = sink.Close()
-
     let boxToMtrx (box: Box) : Matrix3x2 = 
         let bLength = box.b.Length()
         let cLength = box.c.Length()
         let dotProdBC = Vector2.Dot(box.b,box.c)
         let angleBC = acos (dotProdBC / (bLength * cLength )) 
-        let cScale = if float angleBC >= Math.PI || float angleBC <= -Math.PI then
-                        -cLength
-                     else 
+        let delta = box.c.X * box.b.Y - box.c.Y*box.b.X 
+        let cScale = if delta < 0.0f then
                         cLength
+                     else 
+                        -cLength
 
-        let rotAngleB = float32 (Math.Atan2(float box.b.Y, float box.b.X))
         Console.WriteLine(sprintf "Vector a: %A Vector b: %A Vector c: %A " box.a box.b box.c)
-        Console.WriteLine(sprintf "C-skalering: %A   - Vinkel B og C vektor: %f" cScale angleBC)
+        Console.WriteLine(sprintf "delta: %f --- angleBC: %f ----- cScale:  %f" delta angleBC cScale)
+        let rotAngleB = float32 (Math.Atan2(float box.b.Y, float box.b.X))
 //        transform (rotate rotAngleB box.a) geo
         //transform ((rotate rotAngleB box.a) * (scale bLength cLength box.a) * (translate box.a.X box.a.Y)) geo
         let mtrx = (scaleOrigo bLength cScale) * (translate box.a.X box.a.Y) * (rotate rotAngleB box.a)
@@ -105,21 +105,10 @@ let main argv =
     let grouper (factory: Direct2D1.Factory) (geos: Geometry []) = 
         new GeometryGroup(factory, FillMode.Alternate, geos)
 
-    let geoInBox (transform: Box -> Geometry -> Geometry) (geo: Geometry) (box: Box) : Geometry =
-        // kan droppe skew hvis vi ikke trenger det...
-        // roterer bare etter B aksen og skalerer motsatt om c vektoren peker i en annen retning. kan 
-        // lager en matrix3x2 av boxen
-        transform box geo
 
-//    let bmp : Bitmap = new Bitmap()
 //    let bitmapBrush = new BitmapBrush(d2DRenderTarget, LoadBitmap.Load "image.jpg" d2DRenderTarget)
-    let draw (geo: Geometry) =
-        //d2DRenderTarget.DrawGeometry(geo, pinkBrush, strokeWidth = 1.0f)
-        d2DRenderTarget.DrawGeometry(geo, pinkBrush)
-//        d2DRenderTarget.FillGeometry(geo, bitmapBrush)
+    let draw (geo: Geometry) = d2DRenderTarget.DrawGeometry(geo, pinkBrush)
 
-    /// Her ligger nok litt av trikset
-    /// Må bruke denne også i kombinasjoner...?
     let transform : Box -> Geometry -> Geometry = 
       let transformer (factory: Direct2D1.Factory) (box: Box) (geo : Geometry) : Geometry =
         let mtrx = boxToMtrx box
@@ -129,17 +118,24 @@ let main argv =
     let group = grouper d2DFactory 
     let baz = getThings emptyGeo group 
 
-    let f : Picture = geoInBox transform fishGeo 
+    let fish = fishGeo //:> Geometry
+    let f : Picture = fun (box:Box) -> transform box fish 
     let q = baz.flip f 
     RenderLoop.Run(form, fun _ ->
             //d2DRenderTarget.Clear(new Nullable<Interop.RawColor4>(Interop.RawColor4(0.0f, 0.0f, 0.0f, 0.90f)))
             d2DRenderTarget.BeginDraw()
             let b =  { a = Vector(300.0f, 300.0f); 
+                       b = Vector(500.0f, 000.0f);
+                       c = Vector(0.0f, 500.0f)}
+            (*let b =  { a = Vector(300.0f, 300.0f); 
                        b = Vector(500.0f, 100.0f);
-                       c = Vector(-100.0f, 500.0f)}
-            draw (b |> (baz.over f q ))
+                       c = Vector(-100.0f, 500.0f)}*)
+            //draw (b |> (baz.over f q ))
+            //draw (b |> baz.ttile f)
+            draw (b |> baz.squareLimit 3 f)
             d2DRenderTarget.EndDraw()
             (!swapChain).Present(0, PresentFlags.None) |> ignore
+            Console.ReadLine() |> ignore
         )
     printfn "%A" argv
     backBuffer.Dispose()
