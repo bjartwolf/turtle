e46e667e50567a0bba2f7343361a71f137791718
bjorn-einar-bjartnes-4ss <bjartnes@gmail.com>
2023-10-29 11:21:28 +0100

    Update Library.fs

--- Turtles.DirectX/Library.fs
+++ Turtles.DirectX/Library.fs
@@ -117,8 +117,8 @@ let main argv =
                     b = Vector(1500.0f, 000.0f);
                     c = Vector(0.0f, 1500.0f)}
 
-    let f = fun i -> fun (box:Box) -> transform box fish 
-    let pic : Geometry = baz.squareLimit 7 (f (float32 0)) b
+    let f = fun (box:Box) -> transform box fish 
+    let pic : Geometry = baz.squareLimit 7 f b
     let rectBrush = new SolidColorBrush(d2DRenderTarget, Interop.RawColor4(0.0f, 0.0f, 0.0f, 0.10f));
     let rect: Interop.RawRectangleF = Interop.RawRectangleF(0.0f, 0.0f, float32 ScreenRes.x_max, float32 ScreenRes.y_max)
 
